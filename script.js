2f1167a081b1dd5f36cc5c3329c6468dd6ae0574
GideonCove <gideoncove@gmail.com>
2024-05-08 11:17:10 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+
+//Challenge 1
+// let answer = 0
+// let num = 42
+// function makeNegative(num) {
+  
+//   if (num >= 0) {
+//   answer = num * -1
+//   } else{
+//     console.log( answer)
+//   }
+//   console.log(answer)
+// }
+
+// makeNegative(num)
+
+//challenge 2
+
+// const arr = [1,2,3,4,5,-2]
+
+
+// function positiveSum(arr) {
+//  let answer = 0
+//   for (let i = 0; i<arr.length; i++) {
+//     if (arr[i] > 0) {
+//       answer = answer + arr[i]
+//     }
+   
+//   }
+//   return answer
+// }
+
+//challenge 3
+// function square(number) {
+//     return number * number;
+// }
+
+
+//challenge 4
+// function sum(numbers) {
+//     return numbers.reduce((a, b) => a + b, 0);
+// };
